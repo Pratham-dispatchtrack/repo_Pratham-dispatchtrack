bda6dad35771de55b85e4e79acb953c3f29ad511
DT_Pratham <pratham.madhunapanthula@dispatchtrack.com>
2024-06-17 10:45:43 +0530

    changes to 404 and 500

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "/customer_support", to:"customer_support#customer_support"
   get '/', to:'home#home'
   get '/demo'=>'demo#demo'
+
+  get '*path', to: 'application#render_404', via: :all
+  match "/500", to: "application#render_500", via : all
   # Defines the root path route ("/")
   # root "posts#index"
 end
