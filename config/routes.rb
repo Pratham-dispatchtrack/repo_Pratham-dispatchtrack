0b46abc5b1bf55bb34afe647d6c87b9d9d4e2343
DT_Pratham <pratham.madhunapanthula@dispatchtrack.com>
2024-06-17 11:12:41 +0530

    final

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "/customer_support", to:"customer_support#customer_support"
-  get '/', to:'home#home'
+  get '/home', to:'home#home'
   get '/demo'=>'demo#demo'
 
   get '*path', to: 'application#render_404', via:  :all
